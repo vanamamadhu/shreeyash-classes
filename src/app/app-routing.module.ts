2db5651e0e2927e1c347c118dbed4572c53d8cba
vanamamadhu <1emai2madhu@gmail.com>
2021-05-09 19:55:46 +0530

    modifying menu and fonts.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdmindashboardComponent } from './admindashboard/admindashboard.component';
 import { LoginComponent } from './login/login.component';
 import { UserdashboardComponent } from './userdashboard/userdashboard.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent},
   { path: "userdashboard", component: UserdashboardComponent },
-  { path: "login", component: LoginComponent}
+  { path: "login", component: LoginComponent},
+  { path: "admindashboard", component: AdmindashboardComponent}
 ];
 
 @NgModule({
