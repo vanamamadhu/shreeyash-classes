685cef2b95c790224a10b3c9b05f50cf1fa7b427
vanamamadhu <1emai2madhu@gmail.com>
2021-05-10 20:48:47 +0530

    created another page view questions with edit and delete

--- src/app/viewquestions/viewquestions.component.ts
+++ src/app/viewquestions/viewquestions.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewquestionsComponent implements OnInit {
 
+  public questionslist: Array<any>= new Array<any>();
   constructor() { }
 
   ngOnInit() {
+    var j = 0, k = 0;
+    for(var i = 0; i< 20; i++){
+      if(i == i%2){this.questionslist.push("science question "+j); j++;}
+      else{ this.questionslist.push("Maths question "+k); k++;}
+    }
   }
 
 }
