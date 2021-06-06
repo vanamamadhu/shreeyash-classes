fc36879ad53637be96d888fd8b10d024cecc1f72
vanamamadhu <1emai2madhu@gmail.com>
2021-06-06 19:06:52 +0530

    constants

---
+++ src/app/constants.component.ts
@@ -0,0 +1,37 @@
+export class ApplicationConstants {
+    public languages: Array<string> = [
+        "Mathematics",
+        "Science",
+        "Physics",
+        "Chemistry",
+        "Biology"
+    ];
+
+    public batches: Array<string> = [
+        "Batch 1",
+        "Batch 2",
+        "Batch 3",
+        "Batch 4",
+        "Batch 5",
+        "Batch 6",
+        "Batch 7",
+        "Batch 8",
+        "Batch 9",
+        "Batch 10"
+    ];
+
+    public standard: Array<string> = [
+        "1st standard",
+        "2nd standard",
+        "3rd standard",
+        "4th standard",
+        "5th standard",
+        "6th standard",
+        "7th standard",
+        "8th standard",
+        "9th standard",
+        "10th standard",
+        "11th standard",
+        "12th standard"
+    ];
+}
